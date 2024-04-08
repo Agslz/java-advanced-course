e85311a74357d24c2f8f6ecbda920dba5507fa2e
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-08 17:19:12 -0300

    Adding Google clone

---
+++ google-clone/src/main/java/com/google/googleclone/GoogleCloneApplication.java
@@ -0,0 +1,13 @@
+package com.google.googleclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloneApplication.class, args);
+	}
+
+}
